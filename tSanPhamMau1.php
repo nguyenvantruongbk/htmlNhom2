121e9268c148b67c3712450da0cc46e87b1890fe
Nguyen_Van_Truong <truongnv87@gmail.com>
2024-01-23 20:01:25 +0700

    truong

--- tSanPhamMau1.php
+++ tSanPhamMau1.php
@@ -6,6 +6,9 @@
     <title>Document</title>
 </head>
 <body>
-  <h1>san pham mau 1</h1>  
+  <h1>san pham mau 1</h1> 
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat impedit perspiciatis aperiam quaerat tempore consectetur excepturi eveniet officiis! Autem dignissimos recusandae sapiente fugit nihil quidem culpa nisi eaque animi molestiae.
+  </p> 
 </body>
 </html>
