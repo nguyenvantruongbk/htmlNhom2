7fe0b1eaf04ded2b79de9ec8e83e9f539478b717
AnhCuong <cuongyuz@gmail.com>
2024-01-26 00:06:57 +0700

    cuongtest04

--- cTrangChung.php
+++ cTrangChung.php
@@ -7,31 +7,33 @@
     <title>Document</title>
 </head>
 <body>
-<div class="w3-top w3-bar w3-white w3-wide w3-padding w3-card">
-<a href="#home" class="w3-bar-item w3-button"><b>BR</b> Architects</a>
+    <div class="w3-top w3-bar w3-white w3-wide w3-padding w3-card">
+    <a href="#home" class="w3-bar-item w3-button"><b>CuongArt</b>Gallery</a>
+    <div class="w3-right w3-hide-small">
+    <a href="#projects" class="w3-bar-item w3-button">Trang Chủ</a>
+    <a href="#about" class="w3-bar-item w3-button">Tranh Nghệ Thuật</a>
+    <a href="#contact" class="w3-bar-item w3-button">Nghệ Sĩ</a></div></div>
 
-<!-- Float links to the right. Hide them on small screens -->
-<div class="w3-right w3-hide-small">
-<a href="#projects" class="w3-bar-item w3-button">Projects</a>
-<a href="#about" class="w3-bar-item w3-button">About</a>
-<a href="#contact" class="w3-bar-item w3-button">Contact</a>
-</div>
 
-</div>
-
-<!-- Page Start -->
 <div id="home" class="w3-content" style="max-width:1564px">
 
-<!-- Image in Display Container -->
+
 <div class="w3-display-container w3-content" style="max-width:1500px;">
-<img class="w3-image" src="/images/the_starry_night-wallpaper.jpg" alt="Architecture" width="100%">
+<img class="w3-image" src="/images/tranh-son-dau-nghe-thuat-cao-cap-1.jpg" alt="Architecture" width="100%">
 <div class="w3-display-middle w3-margin-top w3-center">
 <h1 class="w3-xxlarge w3-text-white w3-wide">
-<span class="w3-padding w3-black w3-opacity-min"><b>BR</b></span>
-<span class="w3-hide-small w3-text-light-grey">Architects</span>
+<span class="w3-padding w3-black w3-opacity-min"><b>CuongArt</b></span>
+<span class="w3-hide-small w3-text-light-grey">Gallery</span>
 </h1>
+<h1>Tranh Sơn Dầu Đẹp – Tác Phẩm Tranh Sơn Dầu Nghệ Thuật Hiện Đại</h1>
+<p>
+Mỗi tác phẩm nghệ thuật trong các dòng tranh tại Kiệt Tác Nghệ Thuật đều là một tác phẩm nghệ thuật duy nhất, trải qua khâu kiểm định nghiêm ngặt về chất liệu, chủ đề và ý tưởng của người nghệ sĩ kèm theo đó là giấy chứng nhận tranh gốc cùng chữ kỹ của họa sĩ. Do đó, bạn hoàn toàn có thể yên tâm khi đặt mua tranh nguyên bản chất lượng cao khi lựa chọn CuongArt Gallery
+</P>
 </div>
 </div>
+    
+
+
 
 </body>
 </html>
