f7c77153cd0c396df1cd003910fca38d4da90a44
AnhCuong <cuongyuz@gmail.com>
2024-01-25 23:26:24 +0700

    cuong02

--- cTrangChung.php
+++ cTrangChung.php
@@ -3,10 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="" 
+    <link rel="stylesheet" href="css/cTrangChung.css"/> 
     <title>Document</title>
 </head>
 <body>
-    
+<div class="w3-top w3-bar w3-white w3-wide w3-padding w3-card">
+<a href="#home" class="w3-bar-item w3-button"><b>BR</b> Architects</a>
+
+<!-- Float links to the right. Hide them on small screens -->
+<div class="w3-right w3-hide-small">
+<a href="#projects" class="w3-bar-item w3-button">Projects</a>
+<a href="#about" class="w3-bar-item w3-button">About</a>
+<a href="#contact" class="w3-bar-item w3-button">Contact</a>
+</div>
+
+</div>
+
+<!-- Page Start -->
+<div id="home" class="w3-content" style="max-width:1564px">
+
+<!-- Image in Display Container -->
+<div class="w3-display-container w3-content" style="max-width:1500px;">
+<img class="w3-image" src="/images/the_starry_night-wallpaper.jpg" alt="Architecture" width="100%">
+<div class="w3-display-middle w3-margin-top w3-center">
+<h1 class="w3-xxlarge w3-text-white w3-wide">
+<span class="w3-padding w3-black w3-opacity-min"><b>BR</b></span>
+<span class="w3-hide-small w3-text-light-grey">Architects</span>
+</h1>
+</div>
+</div>
+
 </body>
 </html>
